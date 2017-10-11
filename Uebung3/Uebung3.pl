eb4cbbb80477ff72e72c3ae1faa4a01447081230
gebs <adrian.gebs@stud.hslu.ch>
2017-10-11 20:02:26 +0200

    Uebung 3 Weitergemacht

--- Uebung3/Uebung3.pl
+++ Uebung3/Uebung3.pl
@@ -17,17 +17,53 @@ io_fib() :-
             write(Z2).
 
 %2.
-:- dynamic fak_as/2.
+:- dynamic fak_as/2. 
 fak(0,1).
+fak(N,F) :- 
+        fak_as(N,F),
+        atom_concat('(Hinweis: Fakultät von ', N, A),
+        atom_concat(A, ' war gespeichert)', B),
+        write(B).
 fak(N,F) :-
         N > 0,
         N1 is N-1,
-        ( 
-            fak_as(N,F) -> 
-                write('Hinweis: Fakultät von '),
-                write(N),
-                write(' war gespeichert')
-        ;   fak(N1,F1),
-            F is N * F1,
-            asserta(fak_as(N,F))
-        ).
+        fak(N1,F1),
+        F is N * F1,
+        asserta(fak_as(N,F)).
+
+%2. b).
+fak_clear :-
+    retractall(fak_as(_, _)),
+    write('(Hinweis: Alle gespeicherten Werte wurden gelöscht)').
+
+%3 a).
+add_tail(X, [], [X]).
+add_tail(X,[Head|Tail],[Head|L]) :- 
+                        add_tail(X,Tail,L).
+
+%3 b).
+
+
+
+%4
+p(X) :- a(X).
+p(X) :- b(X),!, c(X).
+p(X) :- d(X).
+a(1).
+b(2).
+b(3).
+c(2).
+c(3).
+d(4).
+
+
+%5
+warn(T) :- T < 80, write('Temparatur ok').
+warn(T) :- T > 80, T < 100, write('Temparatur sehr warm').
+warn(T) :- T > 100, write('Temparatur zu heiss').
+
+% Ohne Cut Operator muss darauf geachtet werden dass sich die einzelnen Reglen ausschliessen. Ansonsten gibt es mehrere Lösungen.
+
+mem(X, [X | _]) :- !.
+mem(X, [_| Tail]) :- mem(X, Tail).
+% Der CUT Operator im ersten Prädikat verhindert das Backtracking, und somit die Suche nach weiteren Lösungen.
